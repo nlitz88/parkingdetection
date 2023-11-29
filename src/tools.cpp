e56dba8b10aae0cb86059b4dca6b2ea342f86b76
Nathan Litzinger <nlitz88@gmail.com>
2023-11-28 21:52:58 -0500

    Added two files for implementing utility functions

---
+++ src/tools.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file transform_tools.cpp
+ * @author Nathan Litzinger (nlitzing@andrew.cmu.edu)
+ * @brief Contains implementations of various utility functions for the parking
+ * detection pipeline.
+ * @version 0.1
+ * @date 2023-11-28
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
