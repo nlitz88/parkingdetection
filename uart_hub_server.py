e3ed91c729426d202810f1b1cba864bf9514a3b2
nlitz88 <nlitz88@gmail.com>
2023-12-12 03:50:19 -0500

    Stubbed out UART receiver code

---
+++ uart_hub_server.py
@@ -0,0 +1,15 @@
+
+
+if __name__ == "__main__":
+    
+    # Open a new serial connection with the specified baud rate and any other
+    # parameters.
+
+    # Create a new OCR pipeline.
+
+    # While true...
+    #   receive a new plate
+    #   pass the plate through the OCR pipeline.
+    #   print out resulting text.
+
+    pass
